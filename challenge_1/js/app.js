cf5627ab8969f2d2a1fbeeaf1b4dc102393ece6a
hockey2249 <kgibbons14@gmail.com>
2016-08-14 19:53:09 -0600

    Sending up homework

--- challenge_1/js/app.js
+++ challenge_1/js/app.js
@@ -1,3 +1,1 @@
 console.log('challenge #1 -- app.js loaded!');
-
-// nothing to see here!
--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,25 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("CohortController", CohortController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Kyle Gibbons";
+  this.age = 29;
+  this.city = "South Hero";
+  this.state = "VT";
+  this.shout = function(str){
+    return str.toUpperCase() + "!";
+  };
+}
+
+
+function CohortController(){
+  this.class_name = "WDI";
+  this.enrolled_students = ["Holly", "Greg", "Jamie", "Allison","Matt", "Maddy", "Charlie", "Monica","Kayce", "Nick", "Abe", "Louis"];
+  this.start_date = "6-13-2016";
+  this.end_date = "9-02-2016";
+  this.days_remaining = "21";
+}
+
